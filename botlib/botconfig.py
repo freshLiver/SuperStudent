d386edfa97041fce8fe3ff5e95c9d808141c7fe5
freshLiver <freshliver.cys@gmail.com>
2021-03-14 01:52:22 +0800

    modify logger form & add BotConfig.AUDIO_INPUT_TMP getter

--- botlib/botconfig.py
+++ botlib/botconfig.py
@@ -1,25 +1,25 @@
+import env_file
 from os import path, system
 from pathlib import Path
 
-import env_file
 
 
 class BotConfig :
     """
     Global Configs of This Line Bot Project
     """
-    
+
     # BotConfig.py path obj
     __THIS_FILE = Path(__file__)
-    
+
     # project root dir abs path
     __PROJECT_ROOT = path.abspath(__THIS_FILE.parent.parent)
-    
+
     # abs path for saving audio messages received from user
-    __AUDIO_MESSAGE_TMP_DIR = path.join(__PROJECT_ROOT, "audio_input_tmp")
+    __AUDIO_INPUT_TMP = path.join(__PROJECT_ROOT, "audio_input_tmp")
 
     # try to create input audio message tmp dir
-    system(f"mkdir {__AUDIO_MESSAGE_TMP_DIR}")
+    system(f"mkdir {__AUDIO_INPUT_TMP}")
 
     # abs path of .env file
     __ENV_FILE_PATH = path.join(__PROJECT_ROOT, ".env")
@@ -35,6 +35,7 @@ class BotConfig :
     __PORT = int(7777)
 
 
+
     # --------------------------------------------------------------------------------------------------------
 
     @staticmethod
@@ -45,8 +46,9 @@ class BotConfig :
         :return: absolute path of project root
         """
         return BotConfig.__PROJECT_ROOT
-    
-    
+
+
+
     @staticmethod
     def get_channel_token() -> str :
         """
@@ -55,6 +57,7 @@ class BotConfig :
         :return: channel token string
         """
         return BotConfig.__LINE_CHANNEL_TOKEN
+
 
 
     @staticmethod
@@ -67,6 +70,7 @@ class BotConfig :
         return BotConfig.__LINE_CHANNEL_SECRET
 
 
+
     @staticmethod
     def get_flask_app_port() -> int :
         """
@@ -75,3 +79,28 @@ class BotConfig :
         :return: port number
         """
         return BotConfig.__PORT
+
+
+
+    @staticmethod
+    def get_audio_input_dir() -> str :
+        """
+        get full path of audio input tmp dir
+        
+        :return: audio input tmp dir path
+        """
+        return BotConfig.__AUDIO_INPUT_TMP
+
+
+
+    @staticmethod
+    def file_path_from( target_dir: str, filename: str, postfix = ".input" ) -> str :
+        """
+        get full file path from target dir
+        
+        :param target_dir: full path of target dir
+        :param filename: filename (without parent dir name and postfix)
+        :param postfix: filename postfix
+        :return: file full path
+        """
+        return path.join(target_dir, f"{filename}{postfix}")
--- botlib/botlogger.py
+++ botlib/botlogger.py
@@ -1,38 +1,45 @@
 import logging
 
 
+
 class BotLogger :
     # -------------------------------------------- class variables  --------------------------------------------
-    logging.basicConfig(level = logging.DEBUG, format = "%(message)s")
+    logging.basicConfig(level = logging.INFO, format = "%(message)s")
     __LOGGER = logging.getLogger()
-    
-    
+
+
+
     # -------------------------------------------- class methods  --------------------------------------------
     @staticmethod
     def log_debug( msg: str ) :
-        BotLogger.__LOGGER.debug(f"DEBUG LOG: {msg}")
-    
-    
+        BotLogger.__LOGGER.debug(f"==> DEBUG LOG: {msg}")
+
+
+
     @staticmethod
     def log_info( msg: str ) :
-        BotLogger.__LOGGER.info(f"INFO LOG: {msg}")
-    
-    
+        BotLogger.__LOGGER.info(f"==> INFO LOG: {msg}")
+
+
+
     @staticmethod
     def log_warning( msg: str ) :
-        BotLogger.__LOGGER.warning(f"WARNING LOG: {msg}")
-    
-    
+        BotLogger.__LOGGER.warning(f"==> WARNING LOG: {msg}")
+
+
+
     @staticmethod
     def log_error( msg: str ) :
-        BotLogger.__LOGGER.error(f"ERROR LOG: {msg}")
-    
-    
+        BotLogger.__LOGGER.error(f"==> ERROR LOG: {msg}")
+
+
+
     @staticmethod
     def log_critical( msg: str ) :
-        BotLogger.__LOGGER.critical(f"CRITICAL LOG: {msg}")
-    
-    
+        BotLogger.__LOGGER.critical(f"==> CRITICAL LOG: {msg}")
+
+
+
     @staticmethod
     def log_exception( msg: str ) :
-        BotLogger.__LOGGER.exception(f"EXCEPTION LOG: {msg}", exc_info = True)
+        BotLogger.__LOGGER.exception(f"==> EXCEPTION LOG: {msg}", exc_info = True)
