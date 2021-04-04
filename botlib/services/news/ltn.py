43344760fb99de13f55a2f880f3f2b4b83fd3f0c
freshLiver <freshliver.cys@gmail.com>
2021-04-04 22:52:08 +0800

    fix datetime format error

--- botlib/services/news/ltn.py
+++ botlib/services/news/ltn.py
@@ -3,22 +3,24 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 
+
+
 html_list = []
 title_list = []
 content_list = []
 date_list = []
 
 
-def parse(keyword: list, ty: (datetime, datetime)):
+def parse( keyword: list, ty: (datetime, datetime) ) :
     url = "https://news.ltn.com.tw/list/breakingnews/popular"
 
     response = rq.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     sel = soup.select("a.tit")
-    for ele in sel:
+    for ele in sel :
         html_list.append(ele["href"])
 
-    for link in html_list:
+    for link in html_list :
         url = link
         response = rq.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -30,22 +32,20 @@ def parse(keyword: list, ty: (datetime, datetime)):
         # get date
         s = soup.find("span", "time").text
         s = s.replace(" ", "").replace("\n", "")
-        date = datetime.datetime.strptime(s, '%Y-%m-%d%H:%M')
-        if ty[0] < date < ty[1]:
+        date = datetime.datetime.strptime(s, '%Y/%m/%d%H:%M')
+        if ty[0] < date < ty[1] :
             # get content
-            text_list = soup.find("div", {"class": "text"}).find_all("p", class_="", recursive=False)
+            text_list = soup.find("div", { "class" : "text" }).find_all("p", class_ = "", recursive = False)
             text = "".join(t.text.replace("\r", "").replace("\n", "").replace('</p>', '').replace('<p>', '') for t in text_list)
             text = re.sub('[a-zA-Z]', '', text)
-            if match(text, keyword):
+            if match(text, keyword) :
                 return text
 
     return "找不到相符結果"
 
 
-def match(content: str, keyword: list):
-
-    for key in keyword:
-        if key not in content:
+def match( content: str, keyword: list ) :
+    for key in keyword :
+        if key not in content :
             return False
     return True
-
