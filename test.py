a809a4f684c1f39cbae7d41ca24d4aecee49748e
freshLiver <freshliver.cys@gmail.com>
2021-04-04 20:42:38 +0800

    update test.py & project config

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-# project libs
-from botlib import BotConfig
-from botlib.converter.datetime_converter import DatetimeConverter
 from botlib.semantic_analyzer import SemanticAnalyzer
 from botlib.services import match_service
 
 
 
 if __name__ == '__main__' :
-    text = "十天後成大操場有活動"
+    text = "我想知道中時關於的臺鐵車禍的新聞"
     analyzer = SemanticAnalyzer(text)
     analyzer.parse_content()
     res = match_service(analyzer)
