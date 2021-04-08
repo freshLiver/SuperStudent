1cece9dc0877d071e42c3666c11ee3d44d4aa03c
freshLiver <freshliver.cys@gmail.com>
2021-04-08 20:37:00 +0800

    add numeric datetime cols for matching datetime range with SQL cmd

--- botlib/services/activity/__init__.py
+++ botlib/services/activity/__init__.py
@@ -10,13 +10,19 @@ from botlib.botlogger import BotLogger
 
 def create_activity( content: str, time_range: (datetime, datetime) ) -> str :
     this_dir = Path(__file__).parent
-    db = sqlite3.connect(Path.joinpath(this_dir, "test.db"))
+    db = sqlite3.connect(Path.joinpath(this_dir, "test.db").__str__())
     cursor = db.cursor()
 
     start_datetime = time_range[0].strftime("%Y/%m/%d %H:%M")
     end_datetime = time_range[1].strftime("%Y/%m/%d %H:%M")
+    start_datetime_value = time_range[0].strftime("%Y%m%d%H%M")
+    end_datetime_value = time_range[1].strftime("%Y%m%d%H%M")
 
-    cmd = f"""INSERT OR IGNORE INTO activities VALUES("{content}","{start_datetime}","{end_datetime}")"""
+    cmd = f"""
+        INSERT OR IGNORE INTO activities 
+        VALUES("{content}","{start_datetime_value}","{end_datetime_value}","{start_datetime}","{end_datetime}")
+    """
+
     cursor.execute(cmd)
     db.commit()
     db.close()
@@ -27,39 +33,30 @@ def create_activity( content: str, time_range: (datetime, datetime) ) -> str :
 def search_activity( keywords: list, time_range: (datetime, datetime) ) -> str :
     # open db
     this_dir = Path(__file__).parent
-    db = sqlite3.connect(Path.joinpath(this_dir, "test.db"))
+    db = sqlite3.connect(Path.joinpath(this_dir, "test.db").__str__())
     cursor = db.cursor()
 
     # select all data that that match keywords from db
-    search_cmd = "SELECT * from activities"
-    if keywords != [] :
-        search_cmd += f""" WHERE content LIKE "%{keywords[0]}%" """
-        for kw in keywords[1 :] :
-            search_cmd += f""" AND content LIKE "%{kw}%" """
+    start_datetime_value = time_range[0].strftime("%Y%m%d%H%M")
+    end_datetime_value = time_range[1].strftime("%Y%m%d%H%M")
+
+    search_cmd = f"""
+        SELECT content from activities WHERE 
+            {start_datetime_value} <= end_datetime_value 
+        AND 
+            {end_datetime_value} >= start_datetime_value 
+    """
+
+    # should also match keywords
+    for kw in keywords :
+        search_cmd += f""" AND content LIKE "%{kw}%" """
 
     cursor.execute(search_cmd)
-    results = cursor.fetchall()
 
     response = "找不到活動"
-    for res in results :
-        try :
-            # get time range of this row
-            start_datetime = datetime.datetime.strptime(res[1], "%Y/%m/%d %H:%M")
-            end_datetime = datetime.datetime.strptime(res[2], "%Y/%m/%d %H:%M")
-
-            # check time range
-            if end_datetime < time_range[0] or start_datetime > time_range[1] :
-                continue
-
-            # use first match activity as response
-            else :
-                response = res[0]
-                break
-
-        except TypeError :
-            BotLogger.exception(f"DB TIME RANGE TYPE ERROR : {res}, pass this data.")
-
-        except Exception as e :
-            BotLogger.exception(f"Search Activity Error, {type(e).__name__} => {e}")
+    try :
+        response = cursor.fetchone()[0]
+    except Exception as e :
+        BotLogger.exception(f"Search Activity Error, {type(e).__name__} => {e}")
 
     return response
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from botlib.services import match_service
 
 
 if __name__ == '__main__' :
+    # text = "三月七號台南火車站有發放免費便當的活動"
     text = "三月七日十二點到三月八日台南有什麼活動"
     analyzer = SemanticAnalyzer(text)
     analyzer.parse_content()
