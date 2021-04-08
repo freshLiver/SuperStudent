3e678cc61409b007ada6b2b6d38e7517253961a2
freshLiver <freshliver.cys@gmail.com>
2021-04-08 16:09:46 +0800

    stupidly add and fetch activity from db

--- botlib/services/activity/__init__.py
+++ botlib/services/activity/__init__.py
@@ -15,7 +15,10 @@ def create_activity( content: str, time_range: (datetime, datetime) ) -> str :
     db = sqlite3.connect(Path.joinpath(this_dir, "test.db"))
     cursor = db.cursor()
 
-    cmd = f"""INSERT OR IGNORE INTO activities VALUES("{content}")"""
+    start_datetime = time_range[0].strftime("%Y/%m/%d %H:%M")
+    end_datetime = time_range[1].strftime("%Y/%m/%d %H:%M")
+
+    cmd = f"""INSERT OR IGNORE INTO activities VALUES("{content}","{start_datetime}","{end_datetime}")"""
     cursor.execute(cmd)
     db.commit()
     db.close()
@@ -31,4 +34,42 @@ def search_activity( proper_nouns: list, events: list, time_range: (datetime, da
     BotLogger.debug(f"time_range : {time_range}")
     BotLogger.debug(f"location : {location}")
 
-    return "查詢活動"
+    # open db
+    this_dir = Path(__file__).parent
+    db = sqlite3.connect(Path.joinpath(this_dir, "test.db"))
+    cursor = db.cursor()
+
+    # select all data that that match keywords from db
+    keywords = proper_nouns + events + location
+    conditions = f"""content LIKE "%{keywords[0]}%" """
+    for kw in keywords[1 :] :
+        conditions += f""" AND content LIKE "%{kw}%" """
+
+    search_cmd = f"""SELECT * from activities WHERE {conditions}"""
+
+    cursor.execute(search_cmd)
+    results = cursor.fetchall()
+
+    response = "找不到活動"
+    for res in results :
+        try :
+            # get time range of this row
+            start_datetime = datetime.datetime.strptime(res[1], "%Y/%m/%d %H:%M")
+            end_datetime = datetime.datetime.strptime(res[2], "%Y/%m/%d %H:%M")
+
+            # check time range
+            if end_datetime < time_range[0] or start_datetime > time_range[1] :
+                continue
+
+            # use first match activity as response
+            else :
+                response = res[0]
+                break
+
+        except TypeError :
+            BotLogger.exception(f"DB TIME RANGE TYPE ERROR : {res}, pass this data.")
+
+        except Exception as e :
+            BotLogger.exception(f"Search Activity Error, {type(e).__name__} => {e}")
+
+    return response
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from botlib.services import match_service
 
 
 if __name__ == '__main__' :
-    text = "三月一日到三月二十號成大燃料相關新聞新聞"
+    text = "三月七日成大操場有什麼活動"
     analyzer = SemanticAnalyzer(text)
     analyzer.parse_content()
     res = match_service(analyzer)
