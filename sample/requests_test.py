3d9356201241dad3798f191a38db2237046d9c6e
freshLiver <freshliver.cys@gmail.com>
2021-05-05 01:54:42 +0800

    update sample script for testing

--- sample/requests_test.py
+++ sample/requests_test.py
@@ -7,7 +7,7 @@ from requests import post
 path.append("..")
 
 # proj libs
-from botlib.api.hanlpapi import HanlpAPI
+from botlib.api.hanlpapi import HanlpApi
 
 
 
@@ -16,6 +16,6 @@ if __name__ == '__main__' :
     sentences = ["這是第一句測試句", "所以這就是第二句"]
     custom_dict = ["測試句"]
 
-    result = HanlpAPI.parse_sentences(sentences, custom_dict)
+    result = HanlpApi.parse_sentences(sentences, custom_dict)
 
     print(result)
--- sample/test.py
+++ sample/test.py
@@ -1,5 +1,7 @@
 from sys import path
 
+from botlib import BotConfig
+from botlib.api.hanlpapi import HanlpApi
 from botlib.semantic_analyzer import SemanticAnalyzer
 from botlib.services import match_service
 
@@ -8,9 +10,12 @@ from botlib.services import match_service
 path.append("..")
 
 if __name__ == '__main__' :
+
+    HanlpApi.URL = BotConfig.HANLP_TEST_URL
+
     # text = "三月七號台南火車站有發放免費便當的活動"
     # text = "六月四號成大資工系館會舉辦專題展"
-    text = "今天成大時報的新聞"
+    text = "兩天前自由時報有什麼關於台北101大樓或是台北車站火災的報導"
     analyzer = SemanticAnalyzer(text)
     analyzer.parse_content()
     res = match_service(analyzer)
